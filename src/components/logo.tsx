15b0405cbb878a191f75eeee145bb953dc826f59
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-01-26 14:56:26 -0300

    style: change logo and favicon

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-import { IconNotebook } from '@tabler/icons-react';
+import Image from 'next/image';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-1">
-      <IconNotebook size={26} />
-      <p className="text-xl font-bold">NoteHub</p>
+    <Link href="/" className="flex items-center gap-2">
+      <Image src="/logo.svg" alt="Logo" width={28} height={28} />
+      <p className="text-xl font-semibold">Notehub</p>
     </Link>
   );
 }
