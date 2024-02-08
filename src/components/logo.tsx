4be6873dada7086ec2cc01e5e67038592306be71
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-02-07 21:37:43 -0300

    style: edit logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <Image src="/logo.svg" alt="Logo" width={28} height={28} />
-      <p className="text-xl font-semibold">Notehub</p>
+      <Image src="/logo.svg" alt="Logo" width={30} height={30} />
+      <p className="text-xl font-bold leading-none">NoteHub</p>
     </Link>
   );
 }
