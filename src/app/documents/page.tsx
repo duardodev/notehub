3c0c6212fde750ac79df1ef630025542afec30c9
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-02-09 14:57:13 -0300

    feat: add welcome section

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,5 +1,25 @@
 'use client';
 
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { IconSquareRoundedPlus } from '@tabler/icons-react';
+import { useUser } from '@clerk/nextjs';
+
 export default function Documents() {
-  return <h1>documents</h1>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center gap-4">
+      <Image src="/paper-documents.svg" alt="Documents" width={320} height={320} />
+
+      <h1 className="text-lg font-medium">
+        {user ? `${user.firstName}, bem-vindo ao NoteHub!` : 'Bem-vindo ao NoteHub!'}
+      </h1>
+
+      <Button className="gap-2">
+        <IconSquareRoundedPlus size={18} />
+        Criar um documento
+      </Button>
+    </div>
+  );
 }
