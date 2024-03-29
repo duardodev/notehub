1c7ba43cbd884293f5a1b4d7e72d2ccd238c4d0c
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-03-29 12:09:10 -0300

    fix: fix the undefined query data error

--- src/app/documents/layout.tsx
+++ src/app/documents/layout.tsx
@@ -1,17 +1,47 @@
 import { Metadata } from 'next';
 
 import { ReactNode } from 'react';
+import { LoadingSpinner } from '@/components/loading-spinner';
 import { Navigation } from './components/navigation';
 import { TrashBinModal } from './components/trash-bin-modal';
-import { LoadingSpinner } from '@/components/loading-spinner';
-import { EdgeStoreProvider } from '@/lib/edgestore';
 import { ClerkLoading, ClerkLoaded } from '@clerk/nextjs';
+import { EdgeStoreProvider } from '@/lib/edgestore';
+
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+import {
+  getArchivedDocuments,
+  getChildDocuments,
+  getDocumentById,
+  getDocuments,
+} from '@/actions/get-documents';
 
 export const metadata: Metadata = {
   title: 'Documentos',
 };
 
-export default function DocumentsLayout({ children }: { children: ReactNode }) {
+export default async function DocumentsLayout({ children }: { children: ReactNode }) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['get-documents'],
+    queryFn: getDocuments,
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ['get-child-documents'],
+    queryFn: () => getChildDocuments,
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ['get-archived-documents'],
+    queryFn: getArchivedDocuments,
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ['get-document-by-id'],
+    queryFn: () => getDocumentById,
+  });
+
   return (
     <>
       <ClerkLoading>
@@ -22,12 +52,14 @@ export default function DocumentsLayout({ children }: { children: ReactNode }) {
 
       <ClerkLoaded>
         <div className="h-screen flex">
-          <Navigation />
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            <Navigation />
 
-          <main className="w-screen h-full flex-1 overflow-y-auto absolute md:static">
-            <TrashBinModal />
-            <EdgeStoreProvider>{children}</EdgeStoreProvider>
-          </main>
+            <main className="w-screen h-full flex-1 overflow-y-auto absolute md:static">
+              <TrashBinModal />
+              <EdgeStoreProvider>{children}</EdgeStoreProvider>
+            </main>
+          </HydrationBoundary>
         </div>
       </ClerkLoaded>
     </>
--- src/lib/query-client.tsx
+++ src/lib/query-client.tsx
@@ -1,9 +1,20 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { useState } from 'react';
 
 export function ReactQueryClientProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 6 * 1000,
+            refetchInterval: 6 * 1000,
+          },
+        },
+      })
+  );
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
